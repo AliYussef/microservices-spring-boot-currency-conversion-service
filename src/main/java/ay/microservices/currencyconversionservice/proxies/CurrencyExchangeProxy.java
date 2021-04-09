23dc10e1b13e1d2a9b1453ef2f4ee2abede8382f
aliyussef <a.yussef@outlook.com>
2021-04-09 14:14:32 +0200

    Added feign proxies.

---
+++ src/main/java/ay/microservices/currencyconversionservice/proxies/CurrencyExchangeProxy.java
@@ -0,0 +1,19 @@
+package ay.microservices.currencyconversionservice.proxies;
+
+import ay.microservices.currencyconversionservice.models.CurrencyConversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Created by aliyussef on 09/04/2021
+ */
+@FeignClient(name = "currency-exchange", url = "localhost:8000/api/v1/currencyExchange/")
+public interface CurrencyExchangeProxy {
+
+    @GetMapping("from/{from}/to/{to}")
+    @ResponseStatus(HttpStatus.OK)
+    CurrencyConversion getExchangeValue(@PathVariable String from, @PathVariable String to);
+}
